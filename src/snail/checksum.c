59cbd2e8fe38f9cfea3b2945f8f0498079c47fb6
Henrico J <henrico@ufpr.br>
2025-05-19 15:43:04 -0300

    checksum2.0

--- src/snail/checksum.c
+++ src/snail/checksum.c
@@ -1,14 +1,15 @@
 #include "checksum.h"
 
 uint8_t
-calculate_checksum (const pkg_t *pkg)
+calculate_checksum (const struct pkg *pkg)
 {
 	uint16_t sum = 0;
+	
 	sum += pkg->size;
 	sum += pkg->sequence_number;
 	sum += pkg->type;
 
-	for (int i = 0; i < pkg_size; i++)
+	for (int i = 0; i < pkg->size; i++)
 		sum += pkg->data[i];
 
 	/* Retorna os 8 bits menos significativos */
