577e829d9cc6e878ba4554e9f0382a08a88c74b1
Davi Campos Ribeiro <dcr23@inf.ufpr.br>
2025-05-22 14:55:00 -0300

    add prepare functions

--- src/snail/pkg.c
+++ src/snail/pkg.c
@@ -15,7 +15,7 @@ prepare_pkg (struct pkg *pkg, pkg_t pkg_type, uint8_t seq_num, uint8_t size)
 void  
 prepare_ack_pkg (struct pkg *pkg, uint8_t seq_num, pkg_t pkg_type)
 {
-  prepare_pkg (pkg, ACK, pkg_type, 0);
+  prepare_pkg (pkg, pkg_type, seq_num, 0);
 }
 
 void 
@@ -26,3 +26,41 @@ prepare_data_pkg (struct pkg *pkg, void *buf, uint8_t size)
   memset (pkg->data, 0, MAX_DATA);
   memcpy (pkg->data, buf, min (size, MAX_DATA));
 }
+
+/* recebe o tamanho do arquivo */
+void 
+prepare_size_pkg (struct pkg *pkg, uint32_t size)
+{
+  prepare_pkg (pkg, SIZE, get_seq_num (), sizeof size);
+
+  memset (pkg->data, 0, MAX_DATA);
+  memcpy (pkg->data, &size, sizeof size);
+}
+
+void 
+prepare_treasure_pkg (struct pkg *pkg, pkg_t pkg_type, uint8_t seq_num, char *filename)
+{
+  prepare_pkg (pkg, pkg_type, seq_num, strlen(filename));
+  
+  memset (pkg->data, 0, MAX_DATA);
+  memcpy (pkg->data, filename, strlen(filename));
+}
+
+void 
+prepare_eof_pkg (struct pkg *pkg)
+{
+  prepare_pkg (pkg, END_OF_FILE, get_seq_num, 0);
+}
+
+int 
+ack_pkg (uint8_t seq_num)
+{
+  errno = 0;
+
+  prepare_ack_pkg (&snail.pkg, seq_num, ACK);
+  while (send_pkg (&snail.pkg) == EXIT_FAILURE);
+
+  return EXIT_SUCCESS;
+} 
+
+
