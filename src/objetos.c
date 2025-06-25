fb432b33054278b0d40d98e74073e795a0052805
Henrico J <henrico@ufpr.br>
2025-06-25 02:49:10 -0300

    add opcao de entrada tipos.txt

--- src/objetos.c
+++ src/objetos.c
@@ -20,7 +20,7 @@ void ler_tipos_arquivos_do_usuario() {
   for (int i = 0; i < 8; i++) {
     int tipo = -1;
     while (tipo < 0 || tipo > 2) {
-      printf("Arquivo %d: ", i);
+      printf("Arquivo %d: ", i + 1);
       scanf("%d", &tipo);
       if (tipo < 0 || tipo > 2) {
         printf("Tipo inválido. Tente 0, 1 ou 2.\n");
@@ -28,6 +28,33 @@ void ler_tipos_arquivos_do_usuario() {
     }
     arquivos[i] = tipo;
   }
+}
+
+int carregar_tipos_do_arquivo(const char *path) {
+  FILE *f = fopen(path, "r");
+  if (!f) {
+    perror("Erro ao abrir arquivo de tipos");
+    return -1;
+  }
+
+  int tipo, i = 0;
+  while (i < 8 && fscanf(f, "%d", &tipo) == 1) {
+    if (tipo < 0 || tipo > 2) {
+      fprintf(stderr, "Tipo inválido na linha %d: %d\n", i + 1, tipo);
+      fclose(f);
+      return -1;
+    }
+    arquivos[i++] = tipo;
+  }
+
+  fclose(f);
+
+  if (i != 8) {
+    fprintf(stderr, "Esperado 8 tipos, mas lidos %d.\n", i);
+    return -1;
+  }
+
+  return 0;
 }
 
 char* 
